8f66fc076204b5a6e5aeb28e1cb5e59673ffdc91
alx-program <dateme468@gmail.com>
2023-02-17 08:33:28 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char ch;
+
+	for(ch = 'z'; ch >= 'a'; i--)
+		putchar(a);
+	putchar('\n');
+	return(0);
+}
