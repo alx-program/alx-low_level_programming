8bdf4e75bd639767e3c61fb4d5b184633dd05526
alx-program <122822323+alx-program@users.noreply.github.com>
2023-02-17 16:47:31 +0000

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,22 +9,26 @@ int main(void)
 {
 	int i;
 	int j;
+	int k;
 
-	for (i = 10; i <= 19; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j = 10; j <= 19; j++)
+		for (j = 0; j <= 9; j++)
 		{
-			if ((j % 10) > (i % 10))
+			for (k = 0; k <= 9; k++)
 			{
-				putchar((i % 10) + '0');
-				putchar((j % 10) + '0');
-				if (i != 18 || j != 19)
+				if (k > j && j > i)
 				{
-					putchar(',');
-					putchar(' ');
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					if (i != 7 || j != 8 || k != 9)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
 			}
-
 		}
 	}
 	putchar('\n');
